44935536be0c70befa788f2d3a637d1ec47dbe05
Isaac <isaac.schultz@wsu.edu>
2019-01-23 12:21:49 -0800

    Basic design layout for milestone1 app

--- Milestone1/Form1.Designer.cs
+++ Milestone1/Form1.Designer.cs
@@ -30,37 +30,71 @@
         {
             this.stateDropDown = new System.Windows.Forms.ComboBox();
             this.cityDropDown = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // stateDropDown
             // 
-            this.stateDropDown.Dock = System.Windows.Forms.DockStyle.Top;
             this.stateDropDown.FormattingEnabled = true;
-            this.stateDropDown.Location = new System.Drawing.Point(0, 0);
+            this.stateDropDown.Location = new System.Drawing.Point(122, 0);
             this.stateDropDown.Name = "stateDropDown";
-            this.stateDropDown.Size = new System.Drawing.Size(800, 21);
+            this.stateDropDown.Size = new System.Drawing.Size(678, 21);
             this.stateDropDown.TabIndex = 0;
             this.stateDropDown.SelectedIndexChanged += new System.EventHandler(this.stateDropDown_SelectedIndexChanged);
             // 
             // cityDropDown
             // 
-            this.cityDropDown.Dock = System.Windows.Forms.DockStyle.Top;
             this.cityDropDown.FormattingEnabled = true;
-            this.cityDropDown.Location = new System.Drawing.Point(0, 21);
+            this.cityDropDown.Location = new System.Drawing.Point(122, 21);
             this.cityDropDown.Name = "cityDropDown";
-            this.cityDropDown.Size = new System.Drawing.Size(800, 21);
+            this.cityDropDown.Size = new System.Drawing.Size(678, 21);
             this.cityDropDown.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(800, 402);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "State";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(116, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "City";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.cityDropDown);
             this.Controls.Add(this.stateDropDown);
             this.Name = "Form1";
             this.Text = "Milestone 1 Sample Application";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -68,6 +102,9 @@
 
         private System.Windows.Forms.ComboBox stateDropDown;
         private System.Windows.Forms.ComboBox cityDropDown;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
