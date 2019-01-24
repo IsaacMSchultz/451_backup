70c402f47122c2fc39975d774c7f45e3bd68b6fe
Isaac <isaac.schultz@wsu.edu>
2019-01-24 15:10:02 -0800

    Pulls data from database. Sill working on bugs

--- Milestone1/Form1.cs
+++ Milestone1/Form1.cs
@@ -14,6 +14,8 @@ namespace Milestone1
     public partial class Form1 : Form
     {
 
+        private static string LOGININFO = "Host=localhost; Username=postgres; Password=greatPassword; Database=milestone1db";
+
         public class Business
         {
             public string name { get; set; }
@@ -23,12 +25,12 @@ namespace Milestone1
 
         public Form1()
         {
-            string[] test = new string[] { "test1", "test2", "Arizona" };
-            string[] testCity = new string[] { "Tucson", "Pheonix" };
+            //string[] test = new string[] { "test1", "test2", "Arizona" };
+            //string[] testCity = new string[] { "Tucson", "Pheonix" };
             InitializeComponent();
             initializeDropDowns();
-            stateDropDown.Items.AddRange(test);
-            cityDropDown.Items.AddRange(testCity);
+            //stateDropDown.Items.AddRange(test);
+            //cityDropDown.Items.AddRange(testCity);
         }
 
         private void initializeDropDowns()
@@ -38,13 +40,22 @@ namespace Milestone1
             // update states dropdown menu
 
             // 'using' keyword to auto call dispose when we are done.
-            using (var connection = new NpgsqlConnection("Host=localhost; Username=postgres; Password=greatPassword; Database=milestone1db"))
+            using (var connection = new NpgsqlConnection(LOGININFO))
             {
                 connection.Open();
                 using (var cmd = new NpgsqlCommand())
                 {
-                    
+                    cmd.Connection = connection;
+                    cmd.CommandText = "SELECT distinct state FROM business ORDER BY state;";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            stateDropDown.Items.Add(reader.GetString(0));
+                        }
+                    }
                 }
+                connection.Close();
             }
 
             DataGridViewTextBoxColumn col1 = new DataGridViewTextBoxColumn();
@@ -56,19 +67,63 @@ namespace Milestone1
             col2.HeaderText = "State";
             col2.Width = 255;
             businessGrid.Columns.Add(col2);
+
+            DataGridViewTextBoxColumn col3 = new DataGridViewTextBoxColumn();
+            col3.HeaderText = "city";
+            col3.Width = 255;
+            businessGrid.Columns.Add(col3);
         }
 
         private void stateDropDown_SelectedIndexChanged(object sender, EventArgs e)
         {
+            ComboBox box = (ComboBox)sender; //casts sender as a ComboBox
+
             // need to look in MSD
 
             // query database to get list of cities in the selected state
             // update city dropdown with list
+
+            using (var connection = new NpgsqlConnection(LOGININFO))
+            {
+                connection.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = connection;
+                    cmd.CommandText = "SELECT distinct city FROM business WHERE city == " + box.SelectedItem + " ORDER BY city;";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cityDropDown.Items.Add(reader.GetString(0));
+                        }
+                    }
+                }
+                connection.Close();
+            }
         }
 
         private void cityDropDown_SelectedIndexChanged(object sender, EventArgs e)
         {
             // populate data into businessGrid from database
+
+            using (var connection = new NpgsqlConnection(LOGININFO))
+            {
+                connection.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = connection;
+                    cmd.CommandText = "SELECT * FROM business ORDER BY state;";
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader.GetString(0));
+                            //cityDropDown.Items.Add(reader.GetString(0));
+                        }
+                    }
+                }
+                connection.Close();
+            }
         }
     }
 }
