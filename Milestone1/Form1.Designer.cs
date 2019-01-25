4b157cf8b50457f019d8846fa2f7d133f79f5210
Isaac <isaac.schultz@wsu.edu>
2019-01-25 13:24:57 -0800

    Better looking GUI

--- Milestone1/Form1.Designer.cs
+++ Milestone1/Form1.Designer.cs
@@ -33,7 +33,9 @@
             this.businessGrid = new System.Windows.Forms.DataGridView();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.businessGrid)).BeginInit();
+            this.groupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // stateDropDown
@@ -50,7 +52,7 @@
             // 
             this.cityDropDown.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.cityDropDown.FormattingEnabled = true;
-            this.cityDropDown.Location = new System.Drawing.Point(122, 21);
+            this.cityDropDown.Location = new System.Drawing.Point(122, 20);
             this.cityDropDown.Name = "cityDropDown";
             this.cityDropDown.Size = new System.Drawing.Size(678, 21);
             this.cityDropDown.TabIndex = 1;
@@ -60,13 +62,13 @@
             // 
             this.businessGrid.AllowUserToAddRows = false;
             this.businessGrid.AllowUserToDeleteRows = false;
-            this.businessGrid.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.businessGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.businessGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.businessGrid.Location = new System.Drawing.Point(0, 48);
+            this.businessGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.businessGrid.Location = new System.Drawing.Point(0, 41);
             this.businessGrid.Name = "businessGrid";
             this.businessGrid.ReadOnly = true;
-            this.businessGrid.Size = new System.Drawing.Size(800, 402);
+            this.businessGrid.Size = new System.Drawing.Size(800, 409);
             this.businessGrid.TabIndex = 2;
             // 
             // textBox1
@@ -74,7 +76,7 @@
             this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.textBox1.Location = new System.Drawing.Point(0, 5);
+            this.textBox1.Location = new System.Drawing.Point(6, 3);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(116, 13);
@@ -87,7 +89,7 @@
             this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Cursor = System.Windows.Forms.Cursors.Default;
-            this.textBox2.Location = new System.Drawing.Point(0, 24);
+            this.textBox2.Location = new System.Drawing.Point(6, 23);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(116, 13);
@@ -95,21 +97,34 @@
             this.textBox2.Text = "City";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
+            // groupBox
+            // 
+            this.groupBox.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox.Controls.Add(this.stateDropDown);
+            this.groupBox.Controls.Add(this.textBox2);
+            this.groupBox.Controls.Add(this.cityDropDown);
+            this.groupBox.Controls.Add(this.textBox1);
+            this.groupBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox.Location = new System.Drawing.Point(0, 0);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(800, 41);
+            this.groupBox.TabIndex = 5;
+            this.groupBox.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.businessGrid);
-            this.Controls.Add(this.cityDropDown);
-            this.Controls.Add(this.stateDropDown);
+            this.Controls.Add(this.groupBox);
             this.Name = "Form1";
             this.Text = "Milestone 1 Sample Application";
             ((System.ComponentModel.ISupportInitialize)(this.businessGrid)).EndInit();
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -120,6 +135,7 @@
         private System.Windows.Forms.DataGridView businessGrid;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox;
     }
 }
 
