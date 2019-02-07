a309f81967abb6ae799d48846fd5457666d1b589
Rebecca Rothschild <rar.roths@live.com>
2019-02-07 12:20:06 -0800

    finished parsing code
    Former-commit-id: b7bad6bb34c11c513908b8e4be3fd5b069406e08 [formerly ad28ea6515cfacef44c1f8020fbe518361318afe]
    Former-commit-id: 6fb5cc8504d03812391f4d409666f34456a4d234

---
+++ ParsingTest/ParsingTest/ParsingTest.py
@@ -0,0 +1,115 @@
+import json
+import os
+
+def cleanStr4SQL(s): 
+    return s.replace("'","`").replace("\n"," ")
+
+def parseCheckinData(): #DONE
+    outfile = open('checkins.txt', 'w')
+
+    def print_dict(dictionary, ident = '', braces=1):
+        for key, value in dictionary.items():
+            if isinstance(value, dict):
+                outfile.write(ident + key + ' : ') 
+                print_dict(value, ident, braces+1) 
+            else:
+                outfile.write(ident +'%s : %s' %(key, value) + '\t') 
+                if str(key) == 'business_id': 
+                        outfile.write('\n') 
+
+    with open('yelp_checkin.json', 'r') as f:
+       line = f.readline() 
+    
+       while line:
+          data = json.loads(line)
+          print_dict(data)
+          line = f.readline()
+
+    print("Done parsing checkin data")
+    outfile.close()
+    f.close()
+
+def parseBusinessData(): #DONE
+     outfile = open('business.txt', 'w')
+
+     def print_bussDict(dictionary, ident = '', braces=1):
+
+        for key, value in dictionary.items():
+            if isinstance(value, dict):
+                outfile.write(ident) 
+                print_bussDict(value, ident+'  ', braces+1)
+            else:
+                if 'business_id' in str(key): 
+                   outfile.write('\n')
+                if 'neighborhood' not in key:
+                    outfile.write(cleanStr4SQL(ident+'%s : %s' %(key, value) + '\t'))
+
+     with open('yelp_business.json', 'r') as f:
+       line = f.readline() 
+    
+       while line:
+         data = json.loads(line)
+         print_bussDict(data)
+         line = f.readline()
+
+     print("Done parsing business data")
+     outfile.close()
+     f.close()
+
+def parseUserData(): #DONE
+     outfile = open('users.txt', 'w')
+
+     def print_useDict(dictionary, ident = '', braces=1):
+
+        for key, value in dictionary.items():
+            if isinstance(value, dict):
+                if 'compliment' not in ident:
+                    outfile.write(ident + '\t') 
+                print_useDict(value, ident+'\t', braces+1)
+            else:
+                if 'compliment' not in key and 'elite' not in key and 'cool' not in key:
+                    outfile.write(cleanStr4SQL(ident+'%s : %s' %(key, value) + '\t'))
+                if 'yelping_since' in str(key):
+                   outfile.write('\n')
+
+     with open('yelp_user.json', 'r') as f:
+       line = f.readline() 
+    
+       while line:
+         data = json.loads(line)
+         print_useDict(data)
+         line = f.readline()
+
+     print("Done parsing user data")
+     outfile.close()
+     f.close()
+
+def parseReviewData(): #DONE
+    with open('yelp_review.json', 'r') as f:
+        outfile = open('review.txt', 'w')
+        line = f.readline()
+        input()
+        while line:
+            data = json.loads(line)
+            outfile.write("review_id : " + cleanStr4SQL(data['review_id'])+'\t')
+            outfile.write(cleanStr4SQL("user_id : " + data['user_id'])+'\t')
+            outfile.write(cleanStr4SQL("business_id : " + data['business_id'])+'\t')
+            outfile.write("stars : " + str(data['stars']) + '\t')
+            outfile.write("date : " + str(data['date'])+'\t')
+            outfile.write("text : " + cleanStr4SQL(data['text'])+'\t')
+            outfile.write("useful : " + str(data['useful'])+'\t') 
+            outfile.write("funny : " + str(data['funny'])+'\t') 
+            outfile.write("cool : " + str(data['cool'])+'\t') 
+            outfile.write('\n');
+
+            line = f.readline()
+            
+    print("Done parsing review data")
+    outfile.close()
+    f.close()
+
+parseCheckinData()
+parseBusinessData()
+parseUserData()
+parseReviewData() 
+os.system("pause") 
