f77303f543eda8012197b6dbfbcc9e8df60bab71
Isaac <isaac.schultz@wsu.edu>
2019-03-11 12:26:25 -0700

    Added a second trigger, but broke something too
    Former-commit-id: 65e3be6ea101efcc9a4c013f54be62d6ae5d079d [formerly d6f898c0fdf6e44a732aa1d276d068b391deee20]
    Former-commit-id: 4150f334e0f1db7fb0132c664e635664a2ad0288

--- 451_Milestone2/Milestone2_TRIGGER.sql
+++ 451_Milestone2/Milestone2_TRIGGER.sql
@@ -1,3 +1,9 @@
+-----------------------------------------------------------------------------------------------------------------------------------------------
+-- Team 2
+-- Milestone 2 Triggers
+-- Isaac Schultz, Rebecca Rothschild
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
 CREATE OR REPLACE FUNCTION defineCountReview() RETURNS trigger AS '
 BEGIN
     UPDATE Business
@@ -13,13 +19,17 @@ FOR EACH ROW
 WHEN (NEW.review_id IS NOT NULL)
 EXECUTE PROCEDURE defineCountReview();
 
+-- test
 INSERT INTO Review VALUES ('1111111111111111111111', '2eJEUJIP54tex7T9YOcLSw','1111111111111111111112', 5,'1996-10-07','it was bland',1,2,3);
 SELECT * FROM Business WHERE business_id = '2eJEUJIP54tex7T9YOcLSw';
 
 DROP TRIGGER countReview ON review;
 
-INSERT INTO yelp_user VALUES ('1111111111111111111112', 'bob', 0, 0, 0, 0, 0, 0, 1996-10-07, 1.1, 1.2);
+--INSERT INTO yelp_user VALUES ('1111111111111111111112', 'bob', 0, 0, 0, 0, 0, 0, 1996-10-07, 1.1, 1.2);
 
+-----------------------------------------------------------------------------------------------------------------------------------------------
+-- Update on a checkin, handles counting the num_checkins for the business.
+-----------------------------------------------------------------------------------------------------------------------------------------------
 
 CREATE OR REPLACE FUNCTION defineCountCheckin() RETURNS trigger AS '
 BEGIN
@@ -33,9 +43,37 @@ END
 --test
 SELECT COUNT(count) FROM checkins WHERE checkins.business_id = NEW.business_id GROUP BY count
 
+
 CREATE TRIGGER countCheckin
 AFTER UPDATE ON checkins
 FOR EACH ROW
+EXECUTE PROCEDURE defineCountReview();
+
+-- test 
+UPDATE checkins 
+SET count = count + 1
+WHERE business_id = '2eJEUJIP54tex7T9YOcLSw' AND hour = "1:00" AND day = "Monday";
+SELECT * FROM Business WHERE business_id = '2eJEUJIP54tex7T9YOcLSw';
+
+DROP TRIGGER countCheckin ON checkins;
+
+---------------------------------------------------------------------------------------------------------------------------------------
+-- Increment checkin count for proper hour and time
+---------------------------------------------------------------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION defineIncrementCountCheckin() RETURNS trigger AS '
+BEGIN
+    UPDATE CHECKINS
+    SET num_checkins = SELECT COUNT(count) FROM checkins WHERE checkins.business_id = NEW.business_id GROUP BY count
+    WHERE Business.business_id = NEW.business_id;
+    RETURN NEW;
+END
+' LANGUAGE plpgsql;
+
+CREATE TRIGGER countCheckinIn
+AFTER INSERT ON Review
+FOR EACH ROW
+WHEN (NEW.review_id IS NOT NULL)
 EXECUTE PROCEDURE defineCountReview();
 
 INSERT INTO Review VALUES ('1111111111111111111111', '2eJEUJIP54tex7T9YOcLSw','1111111111111111111112', 5,'1996-10-07','it was bland',1,2,3);
@@ -45,11 +83,26 @@ DROP TRIGGER countReview ON review;
 
 INSERT INTO yelp_user VALUES ('1111111111111111111112', 'bob', 0, 0, 0, 0, 0, 0, 1996-10-07, 1.1, 1.2);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- Stuff that was in the postgres query when we were testing.
 -- duHFBe87uNSXImQmvBhA7Q
-
---Reviewrating
---numCHeckins
-
 /*
 CREATE OR REPLACE FUNCTION defineCountReview() RETURNS trigger AS '
 BEGIN
