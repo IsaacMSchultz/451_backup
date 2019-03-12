6ba71635f65ce27a2d4f2c798c4dfe3261f1f411
Isaac <isaac.schultz@wsu.edu>
2019-03-12 11:11:42 -0700

    walf-way through writing classes
    Former-commit-id: a6d994a1a9200c0953d18ebabbfced1e35988193 [formerly 94d558b65b574660215924654756859f5def9b36]
    Former-commit-id: ad77c184fec4910d129438b54e37f2a31f0fa40d

---
+++ 451_Milestone2/QueryEngine/Business.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace QueryEngine
+{
+    class Business
+    {
+        private string name;
+        private double latitude;
+        private double longitude;
+        private string id;
+        private int coolVotes;
+        private int funnyVotes;
+        private int useFull;
+        private int fans;
+        private int reviewCOunt;
+        private double avgStars;
+    }
+
+    class Attribute
+    {
+
+    }
+
+    class Hours
+    {
+
+    }
+}
