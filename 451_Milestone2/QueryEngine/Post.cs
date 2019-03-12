ad77c184fec4910d129438b54e37f2a31f0fa40d
Isaac <isaac.schultz@wsu.edu>
2019-03-12 11:11:42 -0700

    walf-way through writing classes
    Former-commit-id: 94d558b65b574660215924654756859f5def9b36

---
+++ 451_Milestone2/QueryEngine/Post.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryEngine
+{
+    public abstract class Post
+    {
+        protected string User_Id { get; }
+        protected string Date { get; set; }
+        protected string Text { get; set; }
+        protected int Cool_Votes { get; set; }
+        protected int Funny_Votes { get; set; }
+        protected int Useful_Votes { get; set; }
+
+        public virtual void getDeleted()
+        {
+
+        }
+    }
+
+    public class Review : Post
+    {
+        private string Review_Id { get; set; }
+        private string Business_Id { get; }
+        private int stars { get; set; }
+
+        public override void getDeleted()
+        {
+
+        }
+    }
+
+    public class Reply : Post // Potential Extra Credit
+    {
+        public override void getDeleted()
+        {
+
+        }
+    }
+}
