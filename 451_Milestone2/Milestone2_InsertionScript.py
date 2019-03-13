76d377655dfffcd5e4417afe86f725b712ce636c
rrothsch <rrothsch@eecs.wsu.edu>
2019-03-13 12:55:08 -0700

    Finished Review data inserting
    Former-commit-id: 3a35fd37fc39550fd5bf3ccb686f958b6977224d

--- 451_Milestone2/Milestone2_InsertionScript.py
+++ 451_Milestone2/Milestone2_InsertionScript.py
@@ -11,7 +11,7 @@ def int2BoolStr (value):
     else:
         return 'True'
 
-def insert2BusinessTable():
+def insert2BusinessTable(): #Should have 11481
     #reading the JSON file
     startingTime = time.process_time()
     with open('./yelp_business.JSON','r') as f:    #TODO: update path for the input file
@@ -55,7 +55,7 @@ def insert2BusinessTable():
     #outfile.close()  #uncomment this line if you are writing the INSERT statements to an output file.
     f.close()
 
-def insert2UserTable():
+def insert2UserTable(): #Should have 192999
     #reading the JSON file
     startingTime = time.process_time()
     with open('./yelp_user.JSON','r') as f:    #TODO: update path for the input file
@@ -99,6 +99,51 @@ def insert2UserTable():
     #outfile.close()  #uncomment this line if you are writing the INSERT statements to an output file.
     f.close()
 
+def insert2ReviewTable(): #Should have 416479
+    #reading the JSON file
+    startingTime = time.process_time()
+    with open('./yelp_review.JSON','r') as f:    #TODO: update path for the input file
+        #outfile =  open('./yelp_business.SQL', 'w')  #uncomment this line if you are writing the INSERT statements to an output file.
+        line = f.readline()
+        count_line = 0
+
+        #connect to yelpdb database on postgres server using psycopg2
+        #TODO: update the database name, username, and password
+        try:
+            conn = psycopg2.connect("dbname='milestone2db' user='postgres' host='localhost' password='greatPassword'")
+            #conn = psycopg2.connect("dbname='milestone2db' user='postgres' host='35.230.13.126' password='oiAv4Kmdup8Pd4vd'")
+        except:
+            print('Unable to connect to the database!')
+        cur = conn.cursor()
+
+        while line:
+            data = json.loads(line)
+            # Generate the INSERT statement for the cussent business
+            # TODO: The below INSERT statement is based on a simple (and incomplete) businesstable schema. Update the statement based on your own table schema and
+            # include values for all businessTable attributes                                                                    \/ num_checkins,
+            sql_str = "INSERT INTO Review (review_id, business_id, user_id, review_stars, date, text, useful_vote, funny_vote, cool_vote) " \
+                    "VALUES ('" + cleanStr4SQL(data["review_id"]) + "','" + cleanStr4SQL(data["business_id"]) + "','" + cleanStr4SQL(data["user_id"]) + "','" + \
+                    str(data["stars"]) + "','" + str(data["date"]) + "','" + cleanStr4SQL(data["text"]) + "','" + str(data["useful"]) + "'," + str(data["funny"]) + ",'" + \
+                    str(data["cool"]) + "');"
+            
+            try:
+                cur.execute(sql_str)
+            except Exception as e:
+                print("Insert failed! " + str(e) + "\nOn line: " + str(count_line))
+            conn.commit()
+            # optionally you might write the INSERT statement to a file.
+            # outfile.write(sql_str)
+
+            line = f.readline()
+            count_line +=1
+
+        cur.close()
+        conn.close()
+
+    print("Processed " + str(count_line) + " Entries in " + str(time.process_time() - startingTime) + " seconds")
+    #outfile.close()  #uncomment this line if you are writing the INSERT statements to an output file.
+    f.close()
 
 #insert2BusinessTable()
-insert2UserTable()
+#insert2UserTable()
+insert2ReviewTable()
