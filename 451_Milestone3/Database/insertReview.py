53192d453587200ea6f416b146743cf54cb71d4d
Isaac <isaac.schultz@wsu.edu>
2019-04-12 16:57:33 -0700

    Optmized for cloud
    Former-commit-id: 4d78a40db331b67e6a7ecf244555599381a480b8

--- 451_Milestone3/Database/insertReview.py
+++ 451_Milestone3/Database/insertReview.py
@@ -12,8 +12,8 @@ def int2BoolStr (value):
         return 'True'
 
 try:
-    #conn = psycopg2.connect("dbname='test1' user='postgres' host='localhost' password='greatPassword'")
-    conn = psycopg2.connect("dbname='milestone2db' user='postgres' host='35.230.13.126' password='oiAv4Kmdup8Pd4vd'")
+    conn = psycopg2.connect("dbname='milestone3db' user='postgres' host='localhost' password='greatPassword'")
+    #conn = psycopg2.connect("dbname='milestone2db' user='postgres' host='35.230.13.126' password='oiAv4Kmdup8Pd4vd'")
 except:
     print('Unable to connect to the database!')
 
@@ -24,24 +24,47 @@ with open('./yelp_review.JSON','r') as f:    #TODO: update path for the input fi
     line = f.readline()
     count_line = 0
 
+    sql_str = "INSERT INTO Review (review_id, business_id, user_id, review_stars, date, text, useful_vote, funny_vote, cool_vote) VALUES "
+
     while line:
-        data = json.loads(line)
-        sql_str = "INSERT INTO Review (review_id, business_id, user_id, review_stars, date, text, useful_vote, funny_vote, cool_vote) " \
-                "VALUES ('" + cleanStr4SQL(data["review_id"]) + "','" + cleanStr4SQL(data["business_id"]) + "','" + cleanStr4SQL(data["user_id"]) + "','" + \
+        data = json.loads(line)        
+        sql_str += "('" + cleanStr4SQL(data["review_id"]) + "','" + cleanStr4SQL(data["business_id"]) + "','" + cleanStr4SQL(data["user_id"]) + "','" + \
                 str(data["stars"]) + "','" + str(data["date"]) + "','" + cleanStr4SQL(data["text"]) + "','" + str(data["useful"]) + "'," + str(data["funny"]) + ",'" + \
-                str(data["cool"]) + "');"
-        
-        try:
-            cur.execute(sql_str)
-        except Exception as e:
-            print("Insert failed! " + str(e) + "\nOn line: " + str(count_line))
-        conn.commit()
+                str(data["cool"]) + "'),"
+
+        if count_line % 500 == 499:
+            sql_str = sql_str[:-1] #Remove the last , from the end of the string.
+            sql_str += ";" #add the semicolon to the end of the query
+
+            try:
+                cur.execute(sql_str)
+            except Exception as e:
+                print("Insert failed! " + str(e) + "\nOn line: " + str(count_line))
+            sql_str = "INSERT INTO Review (review_id, business_id, user_id, review_stars, date, text, useful_vote, funny_vote, cool_vote) VALUES "
+            conn.commit()
 
         line = f.readline()
         count_line +=1
+
+    if sql_str != "INSERT INTO Review (review_id, business_id, user_id, review_stars, date, text, useful_vote, funny_vote, cool_vote) VALUES ":
+            sql_str = sql_str[:-1] #Remove the last , from the end of the string.
+            sql_str += ";" #add the semicolon to the end of the query
+
+            try:
+                cur.execute(sql_str)
+            except Exception as e:
+                print("Insert failed! " + str(e) + "\nOn line: " + str(count_line))            
+            conn.commit()
 
 print("Processed " + str(count_line) + " Entries in " + str(time.process_time() - startingTime) + " seconds")
 
 f.close()
 cur.close()
 conn.close()
+
+
+
+# sql_str = "INSERT INTO Review (review_id, business_id, user_id, review_stars, date, text, useful_vote, funny_vote, cool_vote) " \
+#                 "VALUES ('" + cleanStr4SQL(data["review_id"]) + "','" + cleanStr4SQL(data["business_id"]) + "','" + cleanStr4SQL(data["user_id"]) + "','" + \
+#                 str(data["stars"]) + "','" + str(data["date"]) + "','" + cleanStr4SQL(data["text"]) + "','" + str(data["useful"]) + "'," + str(data["funny"]) + ",'" + \
+#                 str(data["cool"]) + "');"
