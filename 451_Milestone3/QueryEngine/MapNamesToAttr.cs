03e2e892c98d4d0d91b978d6c146e8bde8040acf
Isaac <isaac.schultz@wsu.edu>
2019-04-24 13:57:07 -0700

    Moved map class for attributes functionality.
    Former-commit-id: 7bf24be2f8e138926f89a392fd4553d89f8af7c9

--- 451_Milestone3/QueryEngine/MapNamesToAttr.cs
+++ 451_Milestone3/QueryEngine/MapNamesToAttr.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Milestone2App
+namespace SpicyMap
 {
-    class MapNamesToAttrValPair
+    public class MapNamesToAttrValPair
     {
         Dictionary<string, string[]> name = new Dictionary<string, string[]>();
+        List<string> attrList;
 
         public MapNamesToAttrValPair()
         {
+            name = new Dictionary<string, string[]>();
+            attrList = new List<string> { "BusinessAcceptsCreditCards", "RestaurantsReservations", "WheelchairAccessible", "OutdoorSeating", "GoodForKids", "RestaurantsGoodForGroups",
+                "RestaurantsDelivery", "RestaurantsTakeOut", "WiFi", "BikeParking", "breakfast", "brunch", "lunch", "dinner", "dessert", "latenight", "RestaurantsPriceRange2", "1" };
+
             name["Accepts Credit Cards"] = new string[] { "BusinessAcceptsCreditCards", "True" };
             name["Takes Reservations"] = new string[] { "RestaurantsReservations", "True" };
             name["Wheelchair Accessible"] = new string[] { "WheelchairAccessible", "True" };
@@ -24,7 +29,7 @@ namespace Milestone2App
             name["Bike Parking"] = new string[] { "BikeParking", "True" };
             name["Breakfast"] = new string[] { "breakfast", "True" };
             name["Brunch"] = new string[] { "brunch", "True" };
-            name["Lunch"] = new string[] { "lunch", "True" }; //This one is not in the data
+            name["Lunch"] = new string[] { "lunch", "True" };
             name["Dinner"] = new string[] { "dinner", "True" };
             name["Dessert"] = new string[] { "dessert", "True" };
             name["Late Night"] = new string[] { "latenight", "True" };
@@ -32,6 +37,7 @@ namespace Milestone2App
             name["$$"] = new string[] { "RestaurantsPriceRange2", "2" };
             name["$$$"] = new string[] { "RestaurantsPriceRange2", "3" };
             name["$$$$"] = new string[] { "RestaurantsPriceRange2", "4" };
+            
         }
 
         public Tuple<string, string> MapFrom(string key)
@@ -42,6 +48,16 @@ namespace Milestone2App
                 return new Tuple<string, string>(combo[0], combo[1]);
             }
             return new Tuple<string, string>("N/A", "N/A");
+        }
+
+        public bool ContainsKey(string key)
+        {
+            return name.ContainsKey(key);
+        }
+
+        public bool Contains(string key)
+        {
+            return attrList.Contains(key);
         }
     }
 }
